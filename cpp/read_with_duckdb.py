fedf7c3c53c5f2decd8fc8c734b80ea05ea12f0d
phact <estevezsebastian@gmail.com>
2022-12-06 15:41:06 -0500

    duckdb test script

---
+++ cpp/read_with_duckdb.py
@@ -0,0 +1,14 @@
+import duckdb
+# to start an in-memory database
+con = duckdb.connect(database=':memory:')
+con.execute("SELECT count(*) FROM './out.parquet'")
+print(con.fetchall())
+
+con.execute("describe table './out.parquet'")
+print(con.fetchall())
+
+
+
+#con.execute("SELECT * FROM './out.parquet' limit 1000")
+con.execute("SELECT * FROM './out.parquet' limit 1")
+print(con.fetchall())
